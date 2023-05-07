b128dc1fd727a687c14e368938d1652e1e13f44a
elishevaisaacs <elishevaisaacs@gmail.com>
2023-05-07 16:18:13 +0300

    server on air

---
+++ src/main/java/lior/machines/LiorsMachinesApplication.java
@@ -0,0 +1,13 @@
+package lior.machines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiorsMachinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiorsMachinesApplication.class, args);
+	}
+
+}
